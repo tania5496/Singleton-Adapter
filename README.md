85fecd876506eaca6a529eb1594ef0d90c5005bc
tania5496 <87234112+tania5496@users.noreply.github.com>
2023-01-05 15:35:10 +0200

    Add files via upload

---
+++ README.md
@@ -0,0 +1,1 @@
+# lab10 of OOP course in UCU
---
+++ src/main/java/adapter/task2/TwitterUser.java
@@ -0,0 +1,13 @@
+package adapter.task2;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+
+@Getter
+@AllArgsConstructor
+public class TwitterUser {
+    private String email;
+    private String userCountry;
+    private String UserActiveTime;
+}
